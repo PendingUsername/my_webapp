66304f3ea70dc54d24459bb3338d431fbd8f0160
PendingUsername <beastmodepending@gmail.com>
2024-11-14 20:42:20 -0500

    cleaned up backend files

--- backend/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Item
-
-# Register the Item model with the Django admin site
-admin.site.register(Item)
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hello BeyondMD - Backend</h1>")
+    return HttpResponse("<h1>Hello BeyondMD - Backend - This confirms that the backend works. </h1>")
